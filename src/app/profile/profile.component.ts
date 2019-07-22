d522d45bb62dbfa7d93a09ddf513baac3cba0b28
Dakoda Willden <dakoda.willden@hotmail.com>
2019-07-22 12:28:10 -0600

    Fix for Header looking weird in mobile view, Attempt to fix pdf viewer width in safari

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
   public summaryTitle: string = "About Me";
   public summaryPart1: string = "I love to learn and grow, and work hard to contribute my best at any task I'm working on. I\'ve worked on projects in C#, JavaScript, JQuery, HTML, CSS, Angular, and Powershell.";
   public summaryPart2: string = "There are projects that I've done at work, school, and on the side at home. I will provide links to some hosted web projects that I\'ve worked on, links to the code on GitHub, and provide a summary and instructions on how to use them.";
-  constructor() { }
+  private screenHeight : number;
+  private screenWidth : number;
+  constructor() {
+    this.onResize();
+
+   }
 
   ngOnInit() {
+    
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
 }
