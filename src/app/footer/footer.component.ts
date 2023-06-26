0c141669bd3d966cb641fbaadb864d9ab9086b32
Dakoda Willden <dakoda.willden@gmail.com>
2023-06-26 16:35:16 -0600

    Update resume access, picture scss

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  @Input() footerText: string = "Dakoda Willden   2019";
+  @Input() footerText: string = "Dakoda Willden   2023";
 
 
 
